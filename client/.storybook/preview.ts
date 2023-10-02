1d4df4c958a62f756ac6df77f4bc8fb2dfd82e51
brunobaronims <brunobaronims@gmail.com>
2023-10-01 23:45:04 -0300

    Add kubernetes manifests and update deploy.yaml

--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -2,7 +2,6 @@ import type { Preview } from '@storybook/react';
 
 import { withThemeByClassName } from '@storybook/addon-styling';
 
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
 import '../src/app/globals.css';
 
 const preview: Preview = {
